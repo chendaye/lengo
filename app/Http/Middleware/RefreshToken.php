77eba8351478b9e3eeadfa4b5b7de627ec8c9d7b
chendaye666 <chendaye666@gmail.com>
2019-07-30 11:36:57 +0800

    token 默认刷新

--- app/Http/Middleware/RefreshToken.php
+++ app/Http/Middleware/RefreshToken.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Auth;
 use Closure;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
@@ -20,40 +21,41 @@ class RefreshToken extends BaseMiddleware
      */
     public function handle($request, Closure $next)
     {
+        // todo: 测试
         if(false){
             // 测试token刷新
             $token = $this->auth->parseToken()->refresh();
             return $this->setAuthenticationHeader($next($request), $token);
         }
-        //todo： 检查此次请求中是否带有 token
-        $this->checkForToken($request);
-
+        try{
+            //todo： 检查此次请求中是否带有 token
+            $this->checkForToken($request);
+        }catch(UnauthorizedHttpException $e){
+            throw new UnauthorizedHttpException('jwt-auth', '请求头没有提供Token！');
+        }
         try {
             //todo: 检测用户的登录状态，token 是否正常，如果正常则通过
             if ($this->auth->parseToken()->authenticate()) {
                 return $next($request);
             }else{
-                throw new UnauthorizedHttpException('jwt-auth', 'token验证失败！');
+                throw new TokenExpiredException('jwt-auth', 'token验证失败！');
             }
         } catch (TokenExpiredException $exception) {
             try {
                 //todo: token过期
                 $token = $this->auth->parseToken()->refresh();
-                $api = strstr($request->url(), 'admin');
-                if($api){
+                if(strstr($request->url(), 'admin')){
                     // 后台请求
                     Auth::guard('api')->onceUsingId($this->auth->manager()->getPayloadFactory()->buildClaimsCollection()->toPlainArray()['sub']);
                 }else{
                     Auth::guard('client')->onceUsingId($this->auth->manager()->getPayloadFactory()->buildClaimsCollection()->toPlainArray()['sub']);
                 }
-
+                // 在响应头中返回新的 token
+                return $this->setAuthenticationHeader($next($request), $token);
             } catch (JWTException $exception) {
-               //todo: refresh 过期了，用户无法刷新令牌，需要重新登录，加入黑名单了
-                throw new UnauthorizedHttpException('jwt-auth', $exception->getMessage());
+               //todo: refresh 过期了，用户无法刷新令牌，需要重新登录，加入黑名单了 JWTException TokenBlacklistedException
+                throw new UnauthorizedHttpException('jwt-auth', 'Token: 刷新时间已过：'.$exception->getMessage());
             }
         }
-
-        // 在响应头中返回新的 token
-        return $this->setAuthenticationHeader($next($request), $token);
     }
 }
